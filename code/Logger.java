95ad55072f354c83a3d2b20807760a61d3073e7d
OmkarPrabhu08 <ompr2427@colorado.edu>
2018-04-12 10:55:56 -0600

    Created Logger.java

---
+++ code/Logger.java
@@ -0,0 +1,27 @@
+public class Logger {
+
+
+        //create an object of Logger
+        static int LogID;
+        private static Logger log = new Logger();
+
+        //make the constructor private so that this class cannot be
+        //instantiated
+   private Logger(){}
+
+        //Get the only object available
+        public static Logger getLogObject(){
+        return log;
+    }
+
+        public void updateLog(){
+string log;
+   if(log==null){
+
+   }
+   else{//update it in databse and map it with todays date
+
+   }
+   LogID++:
+    }
+}
