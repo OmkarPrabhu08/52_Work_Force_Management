94a368588bbecb3eb01c3cf49bc9eecd4a12b4fe
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-05-01 07:01:47 -0600

    Update PersonalDetails.java

--- PersonalDetails.java
+++ PersonalDetails.java
@@ -1,124 +1,87 @@
-import java.util.regex.*;
-import java.util.Scanner;
-class PersonalDetails {
+package workforce;
 
-    private String name;
-    private int DOB;
-    private String phonenumber;
-    private String emailID;
-    /*This constructor uses setters to all the attributes of this class
-      @params name,DOB,phonenumber,emailID
-      @return void
-     */
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+import javax.persistence.*;
+import java.io.Serializable;
 
-    public PersonalDetails(String name, int DOB, String phonenumber, String emailID) {
-        setName(name);
-        setDOB(DOB);
-        setPhonenumber(phonenumber);
-        setEmailID(emailID);
-    }
-    /*getter method for name attribute
-      @params none
-      @return string
-     */
-
-    public String getName() {
-        return name;
-    }
-    /*setter method for name attribute checks if number of characters
-      in name String is greater than 40
-      @params name
-      @return void
-     */
-
-    public void setName(String name) {
-        if(name.length()>40){
-            throw new IllegalArgumentException("Invalid Name, Must be below 40 characters");
-        }
-        else{
-            this.name = name;
-        }
-    }
-    /*getter method for DateOfBirth
-      @params none
-      @return int
-     */
-    public int getDOB() {
-        return DOB;
-    }
-    /*setter method for DateOfBirth
-      @params none
-      @return string
-     */
-    public void setDOB(int DOB) {
-        this.DOB = DOB;
-    }
-    /*getter method for phonenumber
-      @params none
-      @return string
-     */
-    public String getPhonenumber() {
-        return phonenumber;
-    }
-    /*setter method for phonenumber, sanity check using pattern match
-      @params phonenumber
-      @return void
-     */
-
-    public void setPhonenumber(String phonenumber) {
-        Pattern p = Pattern.compile("(1)?[7-9][0-9]{9}");
-        Matcher m = p.matcher(phonenumber);
-        if(!(m.find() && m.group().equals(phonenumber)))
-        {
-            throw new IllegalArgumentException("Invalid Phone Number");
-        }
-
-        this.phonenumber = phonenumber;
-    }
-    /*getter method for EmailID
-      @params none
-      @return string
-     */
-    public String getEmailID() {
-        return emailID;
-    }
-    /*Setter method for EmailID attribute
-      Checks whether '@' sign is present or not in address
-      also checks for '.' after emailID
-      @params EmailID
-      @return void
-     */
-    public void setEmailID(String emailID) {
-        int i =0;
-        int count = 0;
-        boolean attrue = false;
-        int posattrue = 0;
-        boolean dottrue = false;
-        for (; i < emailID.length(); i++) {
-            if (emailID.charAt(i) == '@') {
-                count++;
-                posattrue = i;
-                if (count == 1) {
-                    attrue = true;
-                }
-            }
-        }
-        for ( i = posattrue,count=0; i < emailID.length(); i++) {
-            if (emailID.charAt(i) == '.') {
-                count++ ;
-            }
-            if(count == 1)
-            {
-                dottrue = true;
-            }
-        }
-        if(attrue&dottrue==true) {
-            this.emailID = emailID;
-
-        }
-        else {
-            throw new IllegalArgumentException("Invalid EmailID");
-        }
-    }
-
+@Entity
+@Table(name = "personaldetails")
+public class PersonalDetails {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="personalId")
+	private int personalId;
+	
+	@Column(name="lastname")
+	private String lastName;
+	
+	@Column(name="firstname")
+	private String firstName;
+	
+	@Column(name="phonenumber")
+	private int phonenumber;
+	
+	@Column(name = "userId")
+	private int userId;
+	
+	
+	public int getUserId()
+	{
+		return userId;
+	}
+	
+	public void setUserId(int userId)
+	{
+		this.userId = userId;
+	}
+	
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public Date getDOB() {
+		return DOB;
+	}
+	public void setDOB(Date dOB) {
+		DOB = dOB;
+	}
+	
+	@Column(name="emailID")
+	private String emailID;
+	
+	@Column(name="DOB")
+	private Date DOB;
+	
+	public int getPersonalId() {
+		return personalId;
+	}
+	public void setPersonalId(int personalId) {
+		this.personalId = personalId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
 }
